ae7a2000acd97eedf36d4fb9ed36f11ba3eed82b
오한별 <suriessay@naver.com>
2019-02-17 23:39:43 +0900

    1. create globalRouter

--- app.js
+++ app.js
@@ -3,29 +3,19 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router"; //전체를 export 한게 아니라 userRouter만 export한것
-
+import userRouter from "./routers/userRouter"; 
+import videoRouter from "./routers/videoRouter"; 
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-
-const handleHome = (req,res)=>res.send('Hello hi');
-const handleA = (req,res)=>res.send('a hii');
-const betweenHome = (req,res,next)=> {
-    console.log("between");
-    next();
-};
 
 app.use(cookieParser()); // session을 다루기 위한 미들웨어
 app.use(bodyParser.json()); // 서버로부터 오는 값을 받기 위한 미들웨어
 app.use(bodyParser.urlencoded({extended : true})); // 서버로부터 오는 값을 받기 위한 미들웨어
 app.use(helmet()); // 보안을 위해 사용 하는 미들웨어
 app.use(morgan("tiny")); // logger 역할을 하는 미들웨어
-app.use(betweenHome); // 모든 라우터에 적용
 
-app.get('/', handleHome);
-
-app.get('/a', handleA);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app; // somebody import my file i give him app object
---
+++ routers/userRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const userRouter = express.Router(); //전체를 export 한게 아니라 userRouter만 export할 때 {}사용
+
+export default userRouter;
