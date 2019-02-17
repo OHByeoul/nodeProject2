cc3c4f29c8c69aa48e76d0fe0738029c009f88fb
오한별 <suriessay@naver.com>
2019-02-17 15:22:38 +0900

    change 4000 using const PORT

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 4000;
 
 app.get('/', function getHome(req,res) {  
     res.send('Hello');
 });
 
-app.listen(4000, function listening() {  
-    console.log("listening now");
+app.listen(PORT, function listening() {  
+    console.log(`listening now : ${PORT}`);
 });
