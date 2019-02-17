1d7197991c9ab6f96380ddc5e4575a76876cea03
오한별 <suriessay@naver.com>
2019-02-17 16:01:52 +0900

    1. 미들웨어 작성

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const PORT = 4000;
 
 const listening = ()=>console.log(`listening now : ${PORT}`);
 const handleHome = (req,res)=>res.send('Hello hi');
+const handleA = (req,res)=>res.send('a hi');
+const betweenHome = (req,res,next)=> {
+    console.log("between");
+    next();
+};
+
+app.use(betweenHome); // 모든 라우터에 적용
 
 app.get('/', handleHome);
+
+app.get('/a', handleA);
 
 app.listen(PORT, listening);
