b43a25e9270f22511a968b1f4e3a3351aa93058c
오한별 <suriessay@naver.com>
2019-02-17 22:53:34 +0900

    1. index.js - add body-parser, cookie-parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -12,6 +15,9 @@ const betweenHome = (req,res,next)=> {
     next();
 };
 
+app.use(cookieParser()); // session을 다루기 위한 미들웨어
+app.use(bodyParser.json()); // 서버로부터 오는 값을 받기 위한 미들웨어
+app.use(bodyParser.urlencoded({extended : true})); // 서버로부터 오는 값을 받기 위한 미들웨어
 app.use(helmet()); // 보안을 위해 사용 하는 미들웨어
 app.use(morgan("tiny")); // logger 역할을 하는 미들웨어
 app.use(betweenHome); // 모든 라우터에 적용
