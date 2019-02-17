c6d6d7748497772ea5470f9571475cd5c8c53216
오한별 <suriessay@naver.com>
2019-02-17 15:19:17 +0900

    first server running

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+const express = require("express");
+const app = express();
+
+app.get('/', function getHome(req,res) {  
+    res.send('Hello');
+});
+
+app.listen(4000, function listening() {  
+    console.log("listening now");
+});
