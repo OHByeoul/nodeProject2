aad76d7cfa2a6d86acf899365ca4f9a52c348bd3
오한별 <suriessay@naver.com>
2019-02-18 20:40:43 +0900

    1. routes - url을 관리하기 위한 파일설정 2. 각 router에 기본 url 셋팅

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter"; 
 import videoRouter from "./routers/videoRouter"; 
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 app.use(cookieParser()); // session을 다루기 위한 미들웨어
 app.use(bodyParser.json()); // 서버로부터 오는 값을 받기 위한 미들웨어
 app.use(bodyParser.urlencoded({extended : true})); // 서버로부터 오는 값을 받기 위한 미들웨어
 app.use(helmet()); // 보안을 위해 사용 하는 미들웨어
-app.use(morgan("tiny")); // logger 역할을 하는 미들웨어
+app.use(morgan("dev")); // logger 역할을 하는 미들웨어
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; // somebody import my file i give him app object
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router(); //전체를 export 한게 아니라 userRouter만 export할 때 {}사용
+
+userRouter.get(routes.users, (req,res)=>res.send("users"));
+userRouter.get(routes.userDetail, (req,res)=>res.send("userDetail"));
+userRouter.get(routes.editProfile, (req,res)=>res.send("editProfile"));
+userRouter.get(routes.changePassword, (req,res)=>res.send("changePassword"));
 
 export default userRouter;
