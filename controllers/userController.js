0847fb4ab9eb23507f836089612ca5e5dc4773de
OHByeoul <suriessay@naver.com>
2019-02-20 17:54:28 +0900

    1. join.pug, login.pug - 템플릿 작성

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
-export const join  = (req,res) => res.send("join");
-export const login = (req,res) => res.send("login");
-export const logout = (req,res) => res.send("logout"); //export 작업을 통해서 각 Router에 단어만 입력하면 자동으로 import 해준다
+export const join  = (req,res) => res.render("join");
+export const login = (req,res) => res.render("login");
+export const logout = (req,res) => res.render("logout"); //export 작업을 통해서 각 Router에 단어만 입력하면 자동으로 import 해준다
 export const userDetail = (req,res) => res.send("userDetail");
 export const editProfile = (req,res) => res.send("editProfile");
 export const changePassword = (req,res) => res.send("changePassword");
