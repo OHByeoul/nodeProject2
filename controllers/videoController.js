3e253fa86033cde1a533d323c82d8857cf500e1d
OHByeoul <suriessay@naver.com>
2019-02-20 10:41:11 +0900

    1. main.pug - 넘겨받는 값 이름 수정

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
 export const home = (req,res) => res.render("home", {pageTitle : "home"});
-export const search = (req,res) => res.render("search", {search : "search"});
-export const upload = (req,res) => res.render("upload", {upload : "upload"});
-export const videoDetail = (req,res) => res.render("videoDetail", {videoDetail : "videoDetail"});
-export const editVideo = (req,res) => res.render("editVideo", {editVideo : "editVideo"});
-export const deleteVideo = (req,res) => res.render("deleteVideo", {deleteVideo : "deleteVideo"});
+export const search = (req,res) => res.render("search", {pageTitle : "search"});
+export const upload = (req,res) => res.render("upload", {pageTitle : "upload"});
+export const videoDetail = (req,res) => res.render("videoDetail", {pageTitle : "videoDetail"});
+export const editVideo = (req,res) => res.render("editVideo", {pageTitle : "editVideo"});
+export const deleteVideo = (req,res) => res.render("deleteVideo", {pageTitle : "deleteVideo"});
