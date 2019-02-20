333b72eea8598b82e3cdb48d7ec0a12d08005ddb
오한별 <suriessay@naver.com>
2019-02-20 23:02:44 +0900

    1. db.js - 데이터가 있다고 가정한 faker db임시설정, 2. videoController.js - 가짜 데이터를 import해서 view에 데이터를 넘겨준다, 3. 템플릿 수정

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id:5678,
+        title:"Video 1",
+        description:"first video",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id:"1234",
+            name:"Byeoul",
+            email:"suri@naver.com"
+        }
+    },
+    {
+        id:324393,
+        title:"Video 2",
+        description:"second video",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id:"1234",
+            name:"Byeoul",
+            email:"suri@naver.com"
+        }
+    },
+    {
+        id:23456,
+        title:"Video 3",
+        description:"third video",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id:"1234",
+            name:"Byeoul",
+            email:"suri@naver.com"
+        }
+    }
+]
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import { upload, editVideo, deleteVideo, home } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req,res)=>res.send('videos'));
-videoRouter.get(routes.upload, (req,res)=>res.send('upload'));
+videoRouter.get(routes.home, home);
+videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, (req,res)=>res.send('videoDetail'));
-videoRouter.get(routes.editVideo, (req,res)=>res.send('editVideo'));
-videoRouter.get(routes.deleteVideo, (req,res)=>res.send('deleteVideo'));
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
