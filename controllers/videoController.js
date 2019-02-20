7d308430d954756c61ba785aaeb49530597783fb
OHByeoul <suriessay@naver.com>
2019-02-20 10:39:25 +0900

    1. videoController.js - 템플릿에 특정 값을 callback으로 전달, 2. main.pug - 템플릿 수정

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
-export const home = (req,res) => res.render("home");
-export const search = (req,res) => res.render("search");
-export const upload = (req,res) => res.render("upload");
-export const videoDetail = (req,res) => res.render("videoDetail");
-export const editVideo = (req,res) => res.render("editVideo");
-export const deleteVideo = (req,res) => res.render("deleteVideo");
+export const home = (req,res) => res.render("home", {pageTitle : "home"});
+export const search = (req,res) => res.render("search", {search : "search"});
+export const upload = (req,res) => res.render("upload", {upload : "upload"});
+export const videoDetail = (req,res) => res.render("videoDetail", {videoDetail : "videoDetail"});
+export const editVideo = (req,res) => res.render("editVideo", {editVideo : "editVideo"});
+export const deleteVideo = (req,res) => res.render("deleteVideo", {deleteVideo : "deleteVideo"});
