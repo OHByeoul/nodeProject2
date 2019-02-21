b4b7326f9d8d2538f5501283b1b15782589a1c6e
OHByeoul <suriessay@naver.com>
2019-02-21 09:26:34 +0900

    1. videoBlock/js - mixin페이지 작성, 2. home.js - 작성된 mixin include

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title:"Video 1",
         description:"first video",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator : {
             id:"1234",
             name:"Byeoul",
@@ -16,7 +16,7 @@ export const videos = [
         title:"Video 2",
         description:"second video",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator : {
             id:"1234",
             name:"Byeoul",
@@ -28,7 +28,7 @@ export const videos = [
         title:"Video 3",
         description:"third video",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator : {
             id:"1234",
             name:"Byeoul",
